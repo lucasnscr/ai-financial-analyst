2128599c6e745a6938cc8b98b81d8312c9be1e14
lucasnscr <lucas.nascimento.scr@hotmail.com>
2024-08-19 17:33:25 +0100

    Add webflux

--- src/main/java/com/lucasnscr/ai_financial_analyst/controller/ChatController.java
+++ src/main/java/com/lucasnscr/ai_financial_analyst/controller/ChatController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
@@ -20,7 +21,7 @@ public class ChatController {
     }
 
     @PostMapping("/question")
-    public ResponseEntity<String> getQuestion(@RequestBody String message){
-        return ResponseEntity.ok().body(chatService.question(message));
+    public Flux<String> getQuestion(@RequestBody String message){
+        return chatService.question(message);
     }
 }
--- src/main/java/com/lucasnscr/ai_financial_analyst/service/ChatService.java
+++ src/main/java/com/lucasnscr/ai_financial_analyst/service/ChatService.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.messages.UserMessage;
-import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.ai.document.Document;
@@ -14,11 +13,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
 
 @Service
 public class ChatService {
@@ -37,22 +36,30 @@ public class ChatService {
         this.vectorStore = vectorStore;
     }
 
-    public String question(String message) {
+    public Flux<String> question(String message) {
         Message systemMessage = getSystemMessage(message);
         UserMessage userMessage = new UserMessage(message);
         Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
-
         log.info("Asking AI model to reply to question.");
-        ChatResponse chatResponse = chatClient.prompt(prompt).call().chatResponse();
-        log.info("AI responded.");
-        return chatResponse.getResults().getFirst().getOutput().getContent();
+        return chatClient.prompt(prompt)
+                .stream()
+                .chatResponse()
+                .map(chatResponse -> chatResponse.getResults()
+                        .getFirst()
+                        .getOutput()
+                        .getContent())
+                .doOnNext(content -> log.info("Received part of the response: {}", content))
+                .doOnComplete(() -> log.info("AI response completed."))
+                .doOnError(error -> log.error("Error during AI response streaming", error));
     }
 
     private Message getSystemMessage(String message) {
         log.info("Retrieving relevant documents");
         List<Document> similarDocuments = vectorStore.similaritySearch(message);
         log.info(String.format("Found %s relevant documents.", similarDocuments.size()));
-        String documents = similarDocuments.stream().map(Document::getContent).collect(Collectors.joining("\n"));
+        String documents = similarDocuments.stream()
+                .map(Document::getContent)
+                .collect(Collectors.joining("\n"));
         SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate(this.qaSystemPromptResource);
         return systemPromptTemplate.createMessage(Map.of("documents", documents));
     }
