8a1db671660b29a1d813f1d4026911bd06744144
lucasnscr <lucas.nascimento.scr@hotmail.com>
2025-02-08 23:58:31 +0000

    remove autowired

--- src/main/java/com/lucasnscr/ai_financial_analyst/controller/LoadDataController.java
+++ src/main/java/com/lucasnscr/ai_financial_analyst/controller/LoadDataController.java
@@ -1,7 +1,6 @@
 package com.lucasnscr.ai_financial_analyst.controller;
 
 import com.lucasnscr.ai_financial_analyst.service.DataLoadingService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +13,6 @@ public class LoadDataController {
 
     private final DataLoadingService dataLoadingService;
 
-    @Autowired
     public LoadDataController(DataLoadingService dataLoadingService){
         this.dataLoadingService = dataLoadingService;
     }
